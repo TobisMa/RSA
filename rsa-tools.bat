cdf1232cdea34da5dc23f52fe57b0d9d484ccf85
TobisMa <tobias-ma@gmx.net>
2023-03-07 13:50:00 +0100

    added more possible win32 python commands as fallbacks

--- rsa-tools.bat
+++ rsa-tools.bat
@@ -1,1 +1,1 @@
-start cmd.exe /k "echo RSA interactive python shell && python -i rsa.py && exit"
+start cmd.exe /k "echo RSA interactive python shell && (py -i rsa.py || python3 -i rsa.py || python -i rsa.py) && exit"
