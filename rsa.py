4d24982fe692970c0b72e25d06b36edcb0740e8b
TobisMa <tobias-ma@gmx.net>
2023-04-30 14:03:40 +0200

    File optimizations and input checks

--- rsa.py
+++ rsa.py
@@ -1,70 +1,99 @@
 import math
 from random import randint
+import sys
+from typing import List, Tuple, Union
 
-def generate_primes(until):
-	primes = [2]
-	checkN = 3
-	sqrtN = 1
-	t = -1
-	while primes[-1] < until:
-		if (checkN % 5 != 0 or checkN == 5):
-			for cprime in primes:
-				t = checkN % cprime
-				if (cprime > sqrtN or t == 0):
-					break
-			if (sqrtN + 1) * (sqrtN + 1) < checkN:
-				sqrtN += 1
-			if t != 0:
-				primes.append(checkN)
-		checkN += 2
-	return primes[:-1]
+HUNDRED_THOUSAND = 100_000
 
-
-def extgcd(a, b):
-	table = []
-	r = -1
-	while r != 0:
-		q = a // b
-		r = a % b
-		table.append([a, b, q, r])
-		a = b
-		b = r
-	table[-1].extend([0, 1])
-	for i in range(2, len(table)+1):
-		table[-i].extend([table[-i+1][5], table[-i+1][4] - table[-i][2] * table[-i+1][5]])
-	
-	print("a\tb\tq\tr\tx\ty")
-	for line in table:
-		print('\t'.join(map(str, line)))		
-	return table[0][-2:]	
-
-def public_key(pN, N):
-	possible_primes = list(filter(lambda x: math.gcd(N,x) == 1, generate_primes(pN)))
-	print("Moegliche primes:", possible_primes)
-	flag = True
-	while flag or math.gcd(e, N) != 1:
-		if not flag:
-			print("Not a valid value for e: %s" % e)
-		e = int(input("Choose: "))
-		flag = False	
-	return (e % pN, N)
-
-def private_key(pN, N, e):
-	xy = extgcd(e, pN)
-	return (xy[0] % pN, N)
+def generate_primes(until: int) -> List[int]:
+    primes = [2]
+    checkN = 3
+    sqrtN = 1
+    t = -1
+    while primes[-1] < until:
+        if (checkN % 5 != 0 or checkN == 5):
+            for cprime in primes:
+                t = checkN % cprime
+                if (cprime > sqrtN or t == 0):
+                    break
+            if (sqrtN + 1) * (sqrtN + 1) < checkN:
+                sqrtN += 1
+            if t != 0:
+                primes.append(checkN)
+        checkN += 2
+    return primes[:-1]
 
 
-def main():
-	p = int(input("Primzahl 1: "))
-	q = int(input("Primzahl 2: "))
-	if p == q:
-		return "p and q may not be equal."
-	N = p * q
-	pN = (p - 1) * (q - 1)
-	
-	pub_key = public_key(pN, N)
-	priv_key = private_key(pN, N, pub_key[0])
-	
-	print("public key:", pub_key)
-	print("private key:", priv_key)
+def extgcd(a: int, b: int) -> List[int]:
+    table = []
+    r = -1
+    while r != 0:
+        q = a // b
+        r = a % b
+        table.append([a, b, q, r])
+        a = b
+        b = r
+    table[-1].extend([0, 1])
+    for i in range(2, len(table)+1):
+        table[-i].extend([table[-i+1][5], table[-i+1][4] - table[-i][2] * table[-i+1][5]])
+    
+    print("a\tb\tq\tr\tx\ty")
+    for line in table:
+        print('\t'.join(map(str, line)))		
+    return table[0][-2:]
 
+def public_key(pN: int, N: int) -> Tuple[int, int]:
+    possible_primes = list(filter(lambda x: math.gcd(N,x) == 1, generate_primes(pN)))
+    print("Primes within range:", possible_primes)
+    flag = True
+    while flag or math.gcd(e, N) != 1:
+        if not flag:
+            print("Not a valid value for e: %s" % e, file=sys.stderr)
+        flag = False	
+        e = int(input("Choose: "))
+        if isinstance(e, float):
+            print("Expecting integer; got %s" % type(e).__name__, file=sys.stderr)
+            flag = True
+        elif e < 0:
+            print("WARNING: negative e; instead e %% %s = %s will be used" % (pN, e % pN))
+            e %= pN
+        elif e <= 1:
+            print("Invalid values for e: zero and one", file=sys.stderr)
+      
+    return (e % pN, N)
+
+def private_key(pN: int, N: int, e: int) -> Tuple[int, int]:
+    xy = extgcd(e, pN)
+    return (xy[0] % pN, N)
+
+
+def main() -> Union[Tuple[Tuple[int, int], Tuple[int, int]], str]:
+    first_million_primes = generate_primes(HUNDRED_THOUSAND)
+    p = int(input("Primzahl 1: "))
+    if p < 0:
+        return "No negative prime"
+    elif p > HUNDRED_THOUSAND:
+        print("WARNING: prime value will not be checked", file=sys.stderr)
+    elif p not in first_million_primes:
+        return "p is not prime"
+
+    q = int(input("Primzahl 2: "))
+    if q < 0:
+        return "No negative prime"
+    elif p > HUNDRED_THOUSAND:
+        print("WARNING: prime value will not be checked", file=sys.stderr)
+    elif p not in first_million_primes:
+        return "q is not prime"
+    elif p == q:
+        return "p and q may not be equal."
+
+    N = p * q
+    pN = (p - 1) * (q - 1)
+    
+    pub_key = public_key(pN, N)
+    priv_key = private_key(pN, N, pub_key[0])
+    
+    print("public key:", pub_key)
+    print("private key:", priv_key)
+
+    return pub_key, priv_key
